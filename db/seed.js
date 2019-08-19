7e8745458ff17f160841988d83a81b593d779ffe
DEE ALLEN <JOEYPBA09@GMAIL.COM>
2019-08-19 15:47:17 -0400

    added seed file

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,32 @@
-const axios = require("axios");
-const fs = require("fs");
 
-axios
- .get(
-    "https://api.unsplash.com/photos?client_id=749be1fc4700d5fc3965c9521aba9e749a701f335f71bf4e613a2a620676f939"
- )
- .then(response => {
-   // handle success
-   console.log(response.data);
-   let stringified = JSON.stringify(response.data);
-   console.log(stringified);
-   fs.writeFile(__dirname + “/photos.json”, stringified, “utf8”, err => {
-     if (err) {
-       console.error(err);
-     } else {
-       console.log(successful);
-     }
-   });
- });
+
+const photocollection = require("../db/photos.json")
+
+const Photos = require("../db/models/Photos")
+
+
+
+
+//mapping through the photo data to get properties 
+var newarr = photocollection.map(newitems => {
+    let newObj = {}
+    newObj.width = newitems.width
+    newObj.height= newitems.height
+    newObj.likes = newitems.likes
+    newObj.description = newitems.decription
+    newObj.user = newitems.user
+    newObj.url =newitems.url
+   
+    
+    
+
+    return newObj
+})
+// console.log(newarr)
+
+
+// Photos.remove({})
+// .then(_=>{
+//     Photo.collection.insert(newarr)
+//     .then(allphotos=>{console.log(allphotos)})
+// })
