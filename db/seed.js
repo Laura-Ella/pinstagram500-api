d3aa1d2ffa04689bd393335064c40edf9d8d9b0e
Laura-Ella <lauraella.robinson@gmail.com>
2019-08-20 10:06:57 -0400

    seeded both dbs

--- db/seed.js
+++ db/seed.js
@@ -3,17 +3,36 @@ const Photos = require("../db/models/Photos")
 const Collection = require("../db/models/Collection")
 const collections = require("../db/collections.json")
 
-Photos.deleteMany({})
-  .then(() => {
-    Photos.create(photos)
-      .then((photo) => {
-        console.log(photo)
-        process.exit()
-      })
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+let newPhotos = photos.map(photo => {
+  let x = {
+    width: photo.width,
+    height: photo.height,
+    likes: photo.likes,
+    description: photo.description,
+    username: photo.user.username,
+    url: photo.urls.raw,
+    tag: ''
+  };
+  return x;
+});
+
+Photos.deleteMany({}).then(newPhoto => {
+  Photos.collection.insert(newPhotos).then(newPhoto => [
+    console.log(newPhoto)
+  ])
+})
+
+// Photos.deleteMany({})
+//   .then(() => {
+//     Photos.create(photos)
+//       .then((photo) => {
+//         console.log(photo)
+//         process.exit()
+//       })
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
   // Collection.deleteMany({})
   // .then(() => {
